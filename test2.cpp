0c25d9c184c0edaa9645ea77465420736aa1dc2c
crashdemons <crashenator@gmail.com>
2021-02-24 17:16:00 -0600

    debug fnv0

--- test2.cpp
+++ test2.cpp
@@ -18,13 +18,24 @@ void fnv0(const char* data, size_t len, char* hexdigest) {//hexdigest should be 
   // implementation : https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function#FNV-0_hash_(deprecated)
   uint512_t hash = 0;
 
+  std::cout<<"fnv_prime: "<<boost::lexical_cast<std::string>(fnv_prime)<<std::endl;
+  std::cout<<"initial hash: "<<boost::lexical_cast<std::string>(hash)<<std::endl;
+
   for (size_t i = 0; i < len; i++) {
     // multiplication by 2^344 + 2^8 + 0x57
     hash *= fnv_prime;
+    std::cout<<"  hash*=prime: "<<boost::lexical_cast<std::string>(hash)<<std::endl;
+
 
     // xor with a byte of data
     hash ^= data[i];
+    std::cout<<"  data[i]: "<<boost::lexical_cast<std::string>((unsigned int)data[i])<<std::endl;
+    std::cout<<"  hash^=data[i]: "<<boost::lexical_cast<std::string>(hash)<<std::endl;
+
+
   }
+  std::cout<<"final hash: "<<boost::lexical_cast<std::string>(hash)<<std::endl;
+
 
   for (unsigned int i = 0; i < 128; i++) {
     hexdigest[127 - i] = nibble2hex(static_cast<unsigned char>(hash) & 0xf);
