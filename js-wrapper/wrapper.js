eeeb645de254d9d3ccda4c90de4ddef5c5517c38
crashdemons <crashenator@gmail.com>
2021-03-03 02:35:06 -0600

    fix memory leak in JS wrapper digest() method fix crashdemons/fnv512-wasm#1

--- js-wrapper/wrapper.js
+++ js-wrapper/wrapper.js
@@ -125,7 +125,7 @@ var fnv512 = {
         var ctx = this.init(digest_size,variant);
         this.update(ctx,input);
         var bytes = this.final(ctx);
-        this.cleanup(input);
+        this.cleanup(ctx);
         return bytes;
     },
     
