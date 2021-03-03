c53933792811596c61abb212864de5f0c779b09e
crashdemons <crashenator@gmail.com>
2021-03-03 02:36:41 -0600

    rebuild with fixes

--- make-wasm.sh
+++ make-wasm.sh
@@ -1,8 +1,8 @@
 rm -rf fnv512-wasm-dist
 mkdir fnv512-wasm-dist
 
-#W_FLAGS="-O3 -flto -DNDEBUG"
-W_FLAGS="-O0 -g -v -s ASSERTIONS=1 -s DISABLE_EXCEPTION_CATCHING=0 --memoryprofiler"
+W_FLAGS="-O3 -flto -DNDEBUG"
+#W_FLAGS="-O0 -g -v -s ASSERTIONS=1 -s DISABLE_EXCEPTION_CATCHING=0 --memoryprofiler"
 echo $W_FLAGS
 emcc $W_FLAGS -s MODULARIZE=1 -s 'EXPORT_NAME="createFnv512Module"' -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' uint512_llvm.cpp fnv512.cpp -o fnv512-wasm-dist/fnv512-wasm.js
 
